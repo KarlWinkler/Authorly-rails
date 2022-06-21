5b92967ecfe88b38a8c12e5037b5bf7657a312e5
karlWinkler <karlw200@gmail.com>
2022-06-20 18:36:50 -0600

    start chapters view

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -1,2 +1,27 @@
 class ChaptersController < ApplicationController
+  def new
+    @chapter = Chapter.new
+    @book = Book.find(params[:book_id])
+    render :new, locals: { chapter: @chapter, book: @book }
+  end
+
+  def create
+    @chapter = Chapter.new(chapter_params)
+  end
+
+  def show
+    @chapter = Chapter.find(params[:id])
+    @book = Book.find(params[:book_id])
+    render :show, locals: { chapter: @chapter, book: @book }
+  end
+
+  private
+
+  def chapter_params
+    {
+      index: Book.chapters.last.index + 1,
+      content: nil,
+      book_id: params[:book_id]
+    }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  resources :books
+  resources :books do
+    resources :chapters, only: [:show, :new, :create]
+  end
 end
