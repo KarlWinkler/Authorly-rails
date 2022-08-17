d4c1959320e0a620ebb385f9a707f2b401049128
karlWinkler <karlw200@gmail.com>
2022-08-16 20:38:29 -0600

    refactor request specs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,6 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+require 'support/request_helpers'
 
 ENV['RAILS_ENV'] ||= 'development'
 
@@ -72,4 +73,9 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace('gem name')
+
+  # Include Devise to simulate authentication in tests
+  config.include Warden::Test::Helpers
+  config.include Devise::TestHelpers, type: :controller
+  config.include DeviseRequestSpecHelpers, type: :request
 end
--- spec/requests/author_request_spec.rb
+++ spec/requests/author_request_spec.rb
@@ -3,6 +3,10 @@ require 'rails_helper'
 RSpec.describe 'Author Requests', type: :request do
   let(:author) { create(:author) }
 
+  before do
+    sign_in author
+  end
+
   describe 'GET /author/show' do
     it 'loads the page' do
       get authors_path
